3da4137f3ded4c4e4714eec54702ff9462af693e
dancost <ddan.costache@gmail.com>
2023-12-05 21:40:14 +0200

    add solution for day 1 puzzle 1 using different method

--- day1/d1p1_solution.py
+++ day1/d1p1_solution.py
@@ -3,16 +3,32 @@ with open("d1p1_input.txt") as in_file:
     input_strings = [line.strip() for line in in_file]
 
 
-def calculate_calibration_values(line):
-    numbers = []
-    for i in range(len(line)):
-        if line[i].isnumeric():
-            numbers.append(line[i])
-    return f"{numbers[0] + numbers[-1]}"
+# def calculate_calibration_values(line):
+#     numbers = []
+#     for i in range(len(line)):
+#         if line[i].isnumeric():
+#             numbers.append(line[i])
+#     return f"{numbers[0] + numbers[-1]}"
+
+# result = 0
+# for item in input_strings:
+#     result += int(calculate_calibration_values(item))
+#
+# print(result)
 
 
-result = 0
-for item in input_strings:
-    result += int(calculate_calibration_values(item))
+def calculate_calibration_values(document):
+    total_sum = 0
+    for line in document:
+        first_digit = next((char for char in line if char.isdigit()), None)
+        last_digit = next((char for char in reversed(line) if char.isdigit()), None)
 
+        if first_digit is not None and last_digit is not None:
+            calibration_value = int(first_digit + last_digit)
+            total_sum += calibration_value
+
+    return total_sum
+
+
+result = calculate_calibration_values(input_strings)
 print(result)
