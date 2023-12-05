94ebae1cd5b63b734b7cead54046fc34d18245e5
dancost <ddan.costache@gmail.com>
2023-12-05 21:00:24 +0200

    add solution for day 1 puzzle 1

---
+++ day1/d1p1_solution.py
@@ -0,0 +1,19 @@
+# https://adventofcode.com/2023/day/1
+
+with open("d1p1_input.txt") as in_file:
+    input_strings = [line.strip() for line in in_file]
+
+
+def calculate_calibration_values(line):
+    numbers = []
+    for i in range(len(line)):
+        if line[i].isnumeric():
+            numbers.append(line[i])
+    return f"{numbers[0] + numbers[-1]}"
+
+
+result = 0
+for item in input_strings:
+    result += int(calculate_calibration_values(item))
+
+print(result)
