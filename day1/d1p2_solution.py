c972f868ffb9d185148bbf5540aa602ac47bcfb5
dancost <ddan.costache@gmail.com>
2023-12-05 22:05:31 +0200

    UPDATE solution for day 1 puzzle 1 and create solution for day 1 puzzle 2

--- day1/d1p2_solution.py
+++ day1/d1p2_solution.py
@@ -3,4 +3,4 @@ from d1p1_solution import calculate_calibration_values
 with open("d1_input.txt") as in_file:
     input_strings = [line.strip() for line in in_file]
 
-result = calculate_calibration_values(input_strings)
+
